21d9c00db0637400e27f401a6ae69b89feb84b4b
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-19 13:04:27 +0200

    af

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@ $nutteloospath = "/framework/level%201/Routing-6b869501/src/";
 ($_SERVER['REQUEST_URI']);
 $shorterPath = (str_replace($nutteloospath, "", $_SERVER['REQUEST_URI']));
 $givenRoute = explode("/", $shorterPath);
-if (!isset($givenRoute[0])){
+if (!isset($givenRoute[0])) {
     die("geen path gegeven");
 }
-if (!isset($givenRoute[1])){
+if (!isset($givenRoute[1])) {
     die("geen method gegeven");
 }
 $controller = $givenRoute[0];
@@ -19,10 +19,10 @@ if (!file_exists($checkControllerPath)) {
     (http_response_code(404));
     die("error 404");
 }
-require_once ($checkControllerPath);
+require_once($checkControllerPath);
 $controllerClass = new $controller;
 
-if (!method_exists($controllerClass, $method)){
+if (!method_exists($controllerClass, $method)) {
     (http_response_code(404));
     die("error 404");
 }
