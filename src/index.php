5154bd5c7c423ff0a2dbd00d0242ca1a3851c721
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-21 18:27:10 +0200

    controllers added

--- src/index.php
+++ src/index.php
@@ -4,11 +4,15 @@ $nutteloospath = "/framework/level%201/Routing-6b869501/src/";
 ($_SERVER['REQUEST_URI']);
 $shorterPath = (str_replace($nutteloospath, "", $_SERVER['REQUEST_URI']));
 $givenRoute = explode("/", $shorterPath);
-if (!isset($givenRoute[0])) {
-    die("geen path gegeven");
+$pathLength = strlen($givenRoute[0]);
+if ($pathLength === 0) {
+    require_once("controllers/homecontroller.php");
+    $home = new home;
+    $home->index();
+    die();
 }
 if (!isset($givenRoute[1])) {
-    die("geen method gegeven");
+    die("no method");
 }
 $controller = $givenRoute[0];
 $method = $givenRoute[1];
@@ -24,7 +28,7 @@ $controllerClass = new $controller;
 
 if (!method_exists($controllerClass, $method)) {
     (http_response_code(404));
-    die("error 404");
+    die("error 404 no method found");
 }
 
 $controllerClass->$method();
